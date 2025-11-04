348d4db70211dc78d7f18d35f3a10f8e0e4ecaa0
Francesco Palazzo <fpa@exigo.ch>
2025-11-04 14:10:57 +0100

    Update mix.exs: refactor release configuration, add runtime_tools, and modularize deps/release definitions

--- mix.exs
+++ mix.exs
@@ -7,20 +7,29 @@ defmodule ChatApp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      releases: [
-        chat_app: [
-          include_executables_for: [:unix],
-          applications: [runtime_tools: :permanent]
-        ]
-      ],
-      deps: []
+      releases: releases(),
+      deps: deps()
     ]
   end
 
   def application do
     [
       mod: {ChatApp.Application, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp releases do
+    [
+      chat_app: [
+        include_executables_for: [:unix],
+        applications: [runtime_tools: :permanent],
+        steps: [:assemble, :tar]
+      ]
     ]
   end
 end
